143d00cacf66d0183096e6ed0c26a4877748e139
cy <cydial89@gmail.com>
2024-10-02 14:58:16 +0900

    커밋 테스트 11

---
+++ src/main/java/com/example/test1010/Test1010Application.java
@@ -0,0 +1,13 @@
+package com.example.test1010;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1010Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test1010Application.class, args);
+    }
+
+}
